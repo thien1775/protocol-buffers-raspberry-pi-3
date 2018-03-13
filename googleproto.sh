b623a563ba1b5c5f4adfbdc5971034c33e56f9e1
thien1775 <thien1775@yahoo.com>
2018-03-13 20:54:33 +0000

    Add files via upload

---
+++ googleproto.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+sudo apt-get update -y
+sudo apt-get upgrade -y
+git clone https://git.com/google/protobuf.git
+cd proto buf
+sudo apt-get install autoconf suto make libtool curl make g++ unzip
+./autogen.sh 
+./configure 
+sudo make -j2
+sudo make -j2
+sudo make install -j2
+sudo ldconfig
+
